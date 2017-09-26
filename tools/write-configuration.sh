3cb31b891f8d88101c25cfd4a0207707585fde1f
Maciek Krawiec <maciek.krawiec@zoho.com>
2017-09-26 23:45:29 +0200

    Simplify install script

--- tools/write-configuration.sh
+++ tools/write-configuration.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/env bash
 
 declare -r ZIGZAG_CONFIG_ROOT=/usr/share/zigzag/ansible/root.yml
-declare force=false
-declare extra_vars=''
 
 write_config()
 {
-  local params=(
-    -i "localhost,"
-    -c local $ZIGZAG_CONFIG_ROOT
-  )
+  shift # first argument is force flag
 
-  if [ -n "$extra_vars" ]; then
-    params+=("--extra-vars $extra_vars")
-  fi
-
-  ansible-playbook "${params[@]}"
+  echo ansible-playbook \
+    -i "localhost," \
+    -c local \
+    "$@" \
+    $ZIGZAG_CONFIG_ROOT
 }
 
 show_warning()
@@ -26,21 +21,11 @@ show_warning()
   exit 1
 }
 
-# set variables based on flags
-while getopts 'v:f' flag; do
-  case "${flag}" in
-    f) force=true ;;
-    v) extra_vars="${OPTARG}" ;;
-    *) error "Unexpected option ${flag}" ;;
-  esac
-done
-
-# run
 if [[ $EUID -ne 0 ]]; then
   echo "You must be a root user" 2>&1
   exit 1
-elif [[ "$force" = false ]]; then
+elif [[ $1 != '-f' ]]; then
   show_warning
 else
-  write_config
+  write_config "$@"
 fi
