1a44acef9358dd6185f4a13dd8165f64c2d562d8
Maciek Krawiec <maciek.krawiec@zoho.com>
2017-10-01 21:08:39 +0200

    Add root check; fix args and typos

--- tools/write-configuration.py
+++ tools/write-configuration.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 import argparse
 import subprocess
 
 CONFIG_ROOT = '/usr/share/zigzag/ansible'
 
-parser = argparse.ArgumentParser(description='Apply default configuration of Zigzag Linux to host sytem')
+parser = argparse.ArgumentParser(description='Apply default configuration of Zigzag Linux to host system')
 parser.add_argument('preset', default='root', type=str, nargs='?', help='name of the configuration preset')
 parser.add_argument('--force', const=True, action='store_const', default=False,
                     help='change actual files (you have been warned)')
@@ -18,8 +19,11 @@ if not args.force:
     sys.exit(sep +
             'This script is used to bootstrap configuration of Zigzag Linux.\n'
             'It will override *everything* to default values. \n'
-            'Check --help for a way to proceeed.\n'
+            'Check --help for a way to proceed.\n'
             + sep)
 
+if os.geteuid() != 0:
+    exit("You need to have root privileges to apply the configuration.")
+
 preset_path = '{}/{}.yml'.format(CONFIG_ROOT, args.preset)
-subprocess.run(['ansible-playbook', '-i "localhost,"' '-c local', preset_path], check=True)
+subprocess.run(['ansible-playbook', '-i', '"localhost,"' '-c', 'local', preset_path], check=True)
