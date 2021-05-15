c7107d0826ae7cd4bac1330d1ca86045a5751d37
Maciek Krawiec <142805+mkrawiec@users.noreply.github.com>
2021-05-15 16:37:18 +0200

    Split build process

--- docker/mkiso.sh
+++ docker/mkiso.sh
@@ -5,16 +5,27 @@ declare -r EXTRA_OPTS=$1
 declare -r DESC_DIR=/kiwi/$2
 declare -r RESULTS_DIR=/kiwi/out
 declare -r BUILD_DIR=/tmp/kiwi-build
+declare -r IMAGE_ROOT_DIR=$BUILD_DIR/build/image-root
 mkdir -p $RESULTS_DIR
 
-# run the build
+if [ ! -d $IMAGE_ROOT_DIR ]; then
 kiwi \
     --color-output \
     --shared-cache-dir=$RESULTS_DIR/cache \
     --logfile=$RESULTS_DIR/build.log \
     $EXTRA_OPTS \
-    system build \
+    system prepare \
     --description $DESC_DIR \
+    --root $IMAGE_ROOT_DIR
+echo "To continue run ./build.sh again; To start over run 'docker volume rm zigzag-build'"
+else
+kiwi \
+    --color-output \
+    --shared-cache-dir=$RESULTS_DIR/cache \
+    --logfile=$RESULTS_DIR/build.log \
+    $EXTRA_OPTS \
+    system create \
+    --root $IMAGE_ROOT_DIR \
     --target-dir $BUILD_DIR
 
 # collect iso files
@@ -23,3 +34,4 @@ mv $BUILD_DIR/*.iso $RESULTS_DIR
 # store reference packages
 cat $BUILD_DIR/*.packages | cut -d '|' -f1 \
     | LC_COLLATE=en_US.utf8 sort > $DESC_DIR/package-reference.txt
+fi
