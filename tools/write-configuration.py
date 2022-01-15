efcaf9e39f31bb757832238f043f911708270e3c
Maciek Krawiec <142805+mkrawiec@users.noreply.github.com>
2022-01-15 15:05:14 +0100

    Save ansible log to file

--- tools/write-configuration.py
+++ tools/write-configuration.py
@@ -5,25 +5,44 @@ import sys
 import argparse
 import subprocess
 
-CONFIG_ROOT = '/usr/share/zigzag/ansible'
+ROOT_PATH = "/usr/share/zigzag/ansible"
+LOG_PATH = "/var/log/zigzag-write-configuration.log"
 
-parser = argparse.ArgumentParser(description='Apply default configuration of Zigzag Linux to host system')
-parser.add_argument('preset', default='root', type=str, nargs='?', help='name of the configuration preset')
-parser.add_argument('--force', const=True, action='store_const', default=False,
-                    help='change actual files (you have been warned)')
+parser = argparse.ArgumentParser(
+    description="Apply default configuration of Zigzag Linux to host system"
+)
+parser.add_argument(
+    "preset",
+    default="root",
+    type=str,
+    nargs="?",
+    help="name of the configuration preset",
+)
+parser.add_argument(
+    "--force",
+    const=True,
+    action="store_const",
+    default=False,
+    help="change actual files (you have been warned)",
+)
 
 args = parser.parse_args()
 
 if not args.force:
-    sep = '\n{}\n'.format('*' * 65)
-    sys.exit(sep +
-            'This script is used to bootstrap configuration of Zigzag Linux.\n'
-            'It will override *everything* to default values. \n'
-            'Check --help for a way to proceed.\n'
-            + sep)
+    sep = "\n{}\n".format("*" * 65)
+    sys.exit(
+        sep + "This script is used to bootstrap configuration of Zigzag Linux.\n"
+        "It will override *everything* to default values. \n"
+        "Check --help for a way to proceed.\n" + sep
+    )
 
 if os.geteuid() != 0:
     exit("You need to have root privileges to apply the configuration.")
 
-preset_path = '{}/{}.yml'.format(CONFIG_ROOT, args.preset)
-subprocess.run(['ansible-playbook', '-i', 'localhost,', '-c', 'local', preset_path], check=True)
+preset_path = "{}/{}.yml".format(ROOT_PATH, args.preset)
+ansible_env = env = dict(os.environ, ANSIBLE_LOG_PATH=LOG_PATH)
+subprocess.run(
+    ["ansible-playbook", "-i", "localhost,", "-c", "local", preset_path],
+    check=True,
+    env=ansible_env,
+)
